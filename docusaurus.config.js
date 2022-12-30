3d1f9972186fc5cf4441ee1fa6594f385c572304
James Wood <woodjames275@gmail.com>
2022-12-30 13:41:46 -0700

    chore: update deployment branch in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   organizationName: 'James-D-Wood', // Usually your GitHub org/user name.
   projectName: 'James-D-Wood.github.io', // Usually your repo name.
   trailingSlash: false,
-
+  deploymentBranch: 'main',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
