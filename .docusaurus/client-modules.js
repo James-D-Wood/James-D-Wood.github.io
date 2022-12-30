2e02f5041b73e7cf2cfd0c3dcfa5419a75e96211
James Wood <woodjames275@gmail.com>
2022-12-30 13:48:19 -0700

    Revert "Deploy website - based on 3d1f9972186fc5cf4441ee1fa6594f385c572304"
    This reverts commit ec6748ba4865719f781bfbb881af49335f741d10.

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/james/Repos/James-D-Wood.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/james/Repos/James-D-Wood.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/james/Repos/James-D-Wood.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/james/Repos/James-D-Wood.github.io/src/css/custom.css'),
+];
