9c8c96feea1ea7b47c0fdcddc104c2d377910a68
James Wood <woodjames275@gmail.com>
2023-01-07 16:55:05 -0700

    feat: remove last of tutorials pages and create base projects page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,15 +54,15 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/jw.png",
+          href: "/docs/projects",
         },
         items: [
           {
             type: "doc",
-            docId: "intro",
+            docId: "projects",
             position: "left",
             label: "Projects",
           },
