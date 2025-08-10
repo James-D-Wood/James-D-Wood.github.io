e7ccf11515078ecff09afca9df8aebcad7b92f55
James Wood <woodjames275@gmail.com>
2025-08-09 17:05:12 -0700

    Deploy website - based on 5afcfefc5e6971ebd4be8d8b61debdca8a6b8907

---
+++ assets/js/56109b8e.be9acfc3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjames_codes=self.webpackChunkjames_codes||[]).push([[6545],{208:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"career"}')}}]);
--- assets/js/9c501428.d59bf50e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkjames_codes=self.webpackChunkjames_codes||[]).push([[5845],{6357:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"work"}')}}]);
